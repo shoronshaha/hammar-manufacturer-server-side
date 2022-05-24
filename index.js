5f9a6d20b39127c2f3d2858a346cfba519fdff36
shoronshaha <smaranshaha@gmail.com>
2022-05-25 02:31:36 +0600

    get tool,testimonial and sepecfic tool details

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000
 
@@ -9,6 +10,50 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eldb7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const toolCollection = client.db('manufacturer').collection('tools');
+        const testimonialCollection = client.db('manufacturer').collection('testimonial');
+
+
+
+
+
+
+
+        app.get('/tool', async (req, res) => {
+            const query = {};
+            const cursor = toolCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+        });
+
+        app.get('/testimonial', async (req, res) => {
+            const query = {};
+            const cursor = testimonialCollection.find(query);
+            const testimonial = await cursor.toArray();
+            res.send(testimonial);
+        });
+
+        app.get('/tool/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const tool = await toolCollection.findOne(query);
+            res.send(tool);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 
